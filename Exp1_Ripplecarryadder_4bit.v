d260ac963c88e6ecc261001a217beaa4c132ba35
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 13:30:48 +0530

    Update Exp1_Ripplecarryadder_4bit.v

--- Exp1_Ripplecarryadder_4bit.v
+++ Exp1_Ripplecarryadder_4bit.v
@@ -1,11 +1,21 @@
-module rca_4bit(a,b,c,s,carry);
-input [3:0]a,b;
-input c;
-output [3:0]s;
-output carry;
-
-
-
-
-
+module rippe_adder(S, Cout, X, Y,Cin);
+ input [3:0] X, Y;// Two 4-bit inputs
+ input Cin;
+ output [3:0] S;
+ output Cout;
+ wire w1, w2, w3;fulladder u1(S[0], w1,X[0], Y[0], Cin);
+ fulladder u2(S[1], w2,X[1], Y[1], w1);
+ fulladder u3(S[2], w3,X[2], Y[2], w2);
+ fulladder u4(S[3], Cout,X[3], Y[3], w3);
+endmodule
+module fulladder(S, Co, X, Y, Ci);
+  input X, Y, Ci;
+  output S, Co;
+  wire w1,w2,w3;
+  //Structural code for one bit full adder
+  xor G1(w1, X, Y);
+  xor G2(S, w1, Ci);
+  and G3(w2, w1, Ci);
+  and G4(w3, X, Y);
+  or G5(Co, w2, w3);
 endmodule
