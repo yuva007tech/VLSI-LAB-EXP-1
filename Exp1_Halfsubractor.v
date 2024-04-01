95eb675245010b16f3ab5bbf479bade89347d14a
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 13:29:16 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,6 @@
-module hs(a,b,difference,borrow);
-input a,b;
-output difference,borrow;
-
-
-
+module half_subtractor(D,Bo,A,B);
+input A,B;
+output D,Bo;
+assign D=A^B;
+assign Bo=(~A)&B;
 endmodule
