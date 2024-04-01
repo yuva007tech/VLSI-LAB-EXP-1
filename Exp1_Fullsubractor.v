e6037079cdd292ee8fd9e13d96ebc6d561bdfd6d
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-04-01 13:27:41 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,11 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
+module full_subtractor(a, b, c,D, Bout);
 
+input a, b, c;
 
+output D, Bout;
+
+assign D = a^b^c;
+
+assign Bout = (~a & b) | (~(a^ b) & c);
 
 endmodule
